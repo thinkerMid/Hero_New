3d9abbfa9891f07d70753cf73abe40b23dc21d0a
thinker <33519948+thinkerMid@users.noreply.github.com>
2021-07-02 18:06:35 +0800

    Create CPlayerShader.cpp

---
+++ Hero/CPlayerShader.cpp
@@ -0,0 +1,19 @@
+#include "CPlayerShader.h"
+#include "CFTTBatchModelManager.h"
+#include "CFTTBatchModelBin.h"
+
+
+
+
+
+int CPlayerShader::SetQuality(int A1, int A2)
+{
+	return 0;
+}
+
+int CPlayerShader::CreateMat(int instance, int index, float *)
+{
+	return 0;
+}
+
+
