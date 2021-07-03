30c4a9fe8bd17f78c513c1b756e9fed4ed9a62c3
thinker <33519948+thinkerMid@users.noreply.github.com>
2021-07-03 17:55:26 +0800

    Create CGfxPitchLinePatch.h

---
+++ Hero/CGfxPitchLinePatch.h
@@ -0,0 +1,35 @@
+#pragma once
+class CGfxPitchLinePatch
+{
+public:
+	int IsBlank();
+	int GetTransform();
+	int GetUV(float *a2, float *a3, float *a4, int *a5);
+public:
+	int field_0;
+	int field_4;
+	int field_8;
+	int field_C;
+	int field_10;
+	int field_14;
+	int field_18;
+	int field_1C;
+	int field_20;
+	int field_24;
+	int field_28;
+	int field_2C;
+	int field_30;
+	int field_34;
+	int field_38;
+	int field_3C;
+	int field_40;
+	int field_44;
+	int field_48;
+	int field_4C;
+	int field_50;
+	int field_54;
+	int field_58;
+	int field_5C;
+
+};
+
